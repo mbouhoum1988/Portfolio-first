a3e4113b5a9dd8267aace104d7b8e81a7717e97a
mbouhoum1988 <mbouhoum1988@gmail.com>
2019-01-30 22:45:14 -0600

    portfolio

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Portfolio
+
+this is my portfolio, it has three main webpages the first one is a home webpage (about me), I talked about myself, and i added a links to my linkedin, my github and stack overflow,
+on linkedin there is my contact information.
+
+on the second webpage i called it portfolio, it's where i added my projects each project has an image and a link on top of the image, if you click on the link it will take you to the project webpage.
+
+the third webpage is about contact information if you want to leave a comment or any suggestion you are welcome to do so.
---
+++ assets/javascript/contact.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyASCY_HlKy2Q2QkpkWGYmF-dkuqr-IbGM8",
+        authDomain: "updated-portfolio-bf3d0.firebaseapp.com",
+        databaseURL: "https://updated-portfolio-bf3d0.firebaseio.com",
+        projectId: "updated-portfolio-bf3d0",
+        storageBucket: "",
+        messagingSenderId: "904374058202"
+      };
+      firebase.initializeApp(config);
+
+      var database = firebase.database();
+
+       $("#submit").on("click", function(){
+
+       var  name = $("#name").val().trim();
+       var  email = $("#email").val().trim();
+       var  message = $("#message").val().trim();
+        
+        var add = {
+            name : name,
+            email : email,
+            message : message,
+            dataAdded: firebase.database.ServerValue.TIMESTAM
+        }
+        database.ref().push(add);
+    });
+        database.ref().on("child_added", function(snapshot){
+            var sp = snapshot.val();
+
+        });
+
+   
+});
